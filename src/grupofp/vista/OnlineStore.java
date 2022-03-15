5fdbeb58eb92f36393332bf0b47b65978f7a4ab3
mparragarci <mparragarci@uoc.edu>
2022-03-15 18:13:50 +0100

    Primer commit

--- src/grupofp/vista/OnlineStore.java
+++ src/grupofp/vista/OnlineStore.java
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 package grupofp.vista;
+import java.util.Scanner;
 
 /**
  *
  * @author Marc
  */
-public class OnlineStore {
-    
+public class OnlineStore 
+    {  
+    public static void main(String[] args)
+    {
+        // Declarar el objeto e inicializar con
+        // el objeto de entrada estándar predefinido
+
+        Scanner sc = new Scanner(System.in);
+
+        // entrada de una cadena
+        System.out.println("Que articulo deseas añadir a la lista? ");
+        String name = sc.nextLine();
+
+      
 }
+}
+
+

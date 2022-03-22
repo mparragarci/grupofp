228fc6d0b9f5d24e5a355206d025da415f9e17f2
mparragarci <mparragarci@uoc.edu>
2022-03-22 18:16:34 +0100

    Nova clase PedirDatos

---
+++ src/grupofp/modelo/PedirDatos.java
@@ -0,0 +1,18 @@
+import java.util.Scanner*;
+package grupofp.modelo;
+
+/**
+ *
+ * @author Marc
+ */
+public class PedirDatos {
+    
+    public static void main (String args[]){]
+    
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Introduce tu nombre: ");
+        String nombre = sc.nextLine();
+        System.out.print ("Introduce tu edad: ");
+    }   String aux = sc.nextLine();
+        int edad = Integer.parseInt(aux);
+}       
